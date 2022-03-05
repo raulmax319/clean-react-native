7f1b8adf45bdf61653ef619957689b711d38e98a
Raul Max <raulmax319@gmail.com>
2022-03-05 14:51:31 -0300

    test: ensure AxiosHttpClient calls axios with correct URL and body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,20 +1,33 @@
 import axios from 'axios';
-import Chance from 'chance';
+import { faker } from '@faker-js/faker';
+import { HttpPostParams } from '~/data/protocols/http';
 import { AxiosHttpClient } from './axios-http-client';
-
-const faker = new Chance();
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const makeRandomObject = (): Record<string, unknown> => {
+  const obj: Record<string, unknown> = {};
+  const keys = faker.datatype.number({ min: 1, max: 5 });
+  for (let i = 0; i < keys; i++) {
+    obj[faker.random.word()] = faker.random.word();
+  }
+  return obj;
+};
+
 const makeSut = (): AxiosHttpClient => new AxiosHttpClient();
 
-describe('AxiosHttpClient', () => {
-  test('Should call axios post with correct URL', async () => {
-    const url = faker.url();
-    const sut = makeSut();
-    await sut.post({ url });
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: makeRandomObject(),
+});
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
+describe('AxiosHttpClient', () => {
+  test('Should call axios post method with correct URL and body', async () => {
+    const request = mockPostRequest();
+    const sut = makeSut();
+    await sut.post(request);
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 });
