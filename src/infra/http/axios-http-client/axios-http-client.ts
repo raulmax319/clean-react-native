b82eff9599f82e53f9325fc01807865329ea6533
Raul Max <raulmax319@gmail.com>
2022-03-05 15:16:58 -0300

    feat: ensure AxiosHttpClient to return the correct status and body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
-import { HttpPostParams } from '~/data/protocols/http';
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+} from '~/data/protocols/http';
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpPostClient<unknown, unknown> {
+  async post(params: HttpPostParams<unknown>): Promise<HttpResponse<unknown>> {
+    const res = await axios.post(params.url, params.body);
+    return {
+      status: res.status,
+      body: res.data,
+    };
   }
 }
