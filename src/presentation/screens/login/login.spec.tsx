8b45ec2cddf460da89364f83f07ec331d996dcba
Raul Max <raulmax319@gmail.com>
2022-03-07 19:21:32 -0300

    test: ensure Login inputs starts with initial states

--- src/presentation/screens/login/login.spec.tsx
+++ src/presentation/screens/login/login.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { PressableProps } from 'react-native';
+import { PressableProps, TextInputProps, ViewProps } from 'react-native';
 import Login from './login';
 import theme from '~/presentation/theme';
 
@@ -16,8 +16,25 @@ describe('Login Screen', () => {
     const loginButton = getByTestId(
       'primary-button',
     ) as unknown as React.ReactElement<PressableProps>;
+    const emailInputComponent = getByTestId(
+      'email-input',
+    ) as unknown as React.ReactElement<ViewProps>;
+    const emailInputChildren = emailInputComponent.props
+      .children as React.ReactNode[];
+    const emailTextInputProps =
+      emailInputChildren[1] as React.ReactElement<TextInputProps>;
+
+    const passwordInputComponent = getByTestId(
+      'password-input',
+    ) as unknown as React.ReactElement<ViewProps>;
+    const passwordInputChildren = passwordInputComponent.props
+      .children as React.ReactNode[];
+    const passwordTextInputProps =
+      passwordInputChildren[1] as React.ReactElement<TextInputProps>;
 
     expect(activityIndicator.children.length).toBe(0);
     expect(loginButton.props.accessibilityState.disabled).toBe(true);
+    expect(emailTextInputProps.props.defaultValue).toBe('');
+    expect(passwordTextInputProps.props.defaultValue).toBe('');
   });
 });
