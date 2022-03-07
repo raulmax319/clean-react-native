cf867d076951bc95ae0bed917fecfe862bdd2614
Raul Max <raulmax319@gmail.com>
2022-03-07 16:24:30 -0300

    feat: store provider and redux config

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -4,19 +4,23 @@ import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
+import { Provider as StoreProvider } from 'react-redux';
 import { enableScreens } from 'react-native-screens';
 import { ThemeProvider } from 'styled-components/native';
 import { RootNavigator } from '../presentation/router';
 import theme from '~/presentation/theme';
+import { rootStore } from './store/store';
 
 enableScreens();
 
 const App: React.FC = () => (
-  <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-    <ThemeProvider theme={theme}>
-      <RootNavigator />
-    </ThemeProvider>
-  </SafeAreaProvider>
+  <StoreProvider store={rootStore}>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <ThemeProvider theme={theme}>
+        <RootNavigator />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  </StoreProvider>
 );
 
 export default App;
