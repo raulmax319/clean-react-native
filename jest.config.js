5bfa0740f5e073f75a521c006164bd435e80e38b
Raul Max <raulmax319@gmail.com>
2022-03-07 14:12:28 -0300

    chore: add react native testing lib

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
+  preset: 'react-native',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts)$': 'ts-jest',
+    '^.+\\.(tsx)$': 'babel-jest',
   },
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1',
   },
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
