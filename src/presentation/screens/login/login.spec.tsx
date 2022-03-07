33b3adfd1c69bd43652fdd22def45e02aea1eeed
Raul Max <raulmax319@gmail.com>
2022-03-07 17:39:52 -0300

    test: ensure Login starts with correct initial states

--- src/presentation/screens/login/login.spec.tsx
+++ src/presentation/screens/login/login.spec.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import Login from './login';
 import { ThemeProvider } from 'styled-components/native';
+import { PressableProps } from 'react-native';
+import Login from './login';
 import theme from '~/presentation/theme';
 
 describe('Login Screen', () => {
-  test('Should not render ActivityIndicator on start', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
         <Login />
       </ThemeProvider>,
     );
     const activityIndicator = getByTestId('activity-indicator');
+    const loginButton = getByTestId(
+      'primary-button',
+    ) as unknown as React.ReactElement<PressableProps>;
 
     expect(activityIndicator.children.length).toBe(0);
+    expect(loginButton.props.accessibilityState.disabled).toBe(true);
   });
 });
