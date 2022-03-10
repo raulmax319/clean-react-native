578c2dbd0e356bc5634e6d9d86faab414d3b9c03
Raul Max <raulmax319@gmail.com>
2022-03-10 13:44:02 -0300

    refactor: move input styles to the main component

--- src/presentation/components/input/input.styles.ts
+++ src/presentation/components/input/input.styles.ts
@@ -1,9 +1,8 @@
-import { createElement } from 'react';
-import { Animated, TextInputProps, View } from 'react-native';
+import { Animated, TextInputProps } from 'react-native';
 import { TextInput as RNTextInput } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-const Input = styled(RNTextInput).attrs<TextInputProps>((props) => ({
+export const TextInput = styled(RNTextInput).attrs<TextInputProps>((props) => ({
   placeholderTextColor: props.theme.colors.placeholder,
   selectionColor: props.theme.colors.primary,
   autoCapitalize: 'none',
@@ -12,13 +11,8 @@ const Input = styled(RNTextInput).attrs<TextInputProps>((props) => ({
   font-size: 20px;
   font-weight: 900;
   color: #fdfdfd;
-`;
 
-export const TextInput = styled(View).attrs<TextInputProps>(
-  (props: TextInputProps) => ({
-    children: createElement(Input, props),
-  }),
-)<TextInputProps>`
+  /* ViewProps */
   padding: 20px;
   width: 100%;
   height: 60px;
--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { TextInputProps, View } from 'react-native';
+import { InputState, useLoginContext } from '~/presentation/contexts';
 import { Label, TextInput } from './input.styles';
 
-type InputProps = {
+export type InputProps = {
   label: string;
 } & TextInputProps;
 
 const Input: React.FC<InputProps> = ({ label, ...otherProps }) => {
+  const { inputState, handleInput } = useLoginContext();
+
   return (
     <View testID={`${label.toLowerCase()}-input`}>
       <Label>{label}</Label>
-      <TextInput {...otherProps} />
+      <TextInput
+        defaultValue={inputState[label.toLowerCase() as keyof InputState]}
+        onChangeText={(text) => handleInput({ [label.toLowerCase()]: text })}
+        {...otherProps}
+      />
     </View>
   );
 };
