741d003ff53ee0d33fce3a2f8044e2b17706f72d
Raul Max <raulmax319@gmail.com>
2022-03-15 13:09:46 -0300

    chore: async storage jest mocks

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,10 @@ module.exports = {
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1',
   },
-  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    './__mocks__/mock-async-storage.js',
+  ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
