738136f34aadd98ad5272679233693122e3808e9
Raul Max <raulmax319@gmail.com>
2022-03-15 13:19:56 -0300

    refactor: set the logic inside the hook and return void

--- src/presentation/hooks/use-authentication/use-authentication.ts
+++ src/presentation/hooks/use-authentication/use-authentication.ts
@@ -1,28 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState } from 'react';
-import { HttpPostClient } from '~/data/protocols/http';
-import { RemoteAuthentication } from '~/data/usecases/authentication/remote-authentication';
-import { AccountModel } from '~/domain/models';
-import { AuthenticationParams } from '~/domain/usecases';
+import { Authentication, AuthenticationParams } from '~/domain/usecases';
 
-type RequestFn = (params: AuthenticationParams) => Promise<AccountModel>;
+type Request = (
+  authentication: Authentication,
+) => [boolean, (params: AuthenticationParams) => Promise<void>];
 
-type Request = (client: HttpPostClient) => [boolean, RequestFn];
-
-export const useAuthentication: Request = (client) => {
+export const useAuthentication: Request = (authentication) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const authentication = new RemoteAuthentication(
-    'https://run.mocky.io/v3/993f16e1-53d9-4449-8987-88eef648f67a',
-    client,
-  );
 
   const makePostRequest = async (params: AuthenticationParams) => {
     try {
       setLoading(true);
       const response = await authentication.auth(params);
-
-      return response;
+      void AsyncStorage.setItem('accessToken', response.accessToken);
     } catch (error) {
-      return Promise.reject(error);
+      void Promise.reject(error);
     } finally {
       setLoading(false);
     }
