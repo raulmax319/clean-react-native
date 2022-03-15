c6fa578240debd1edbb18864e4ad7a353b11d007
Raul Max <raulmax319@gmail.com>
2022-03-15 13:22:22 -0300

    refactor: mocking useAuthentication hook for consistent tests

--- src/presentation/screens/login/login.spec.tsx
+++ src/presentation/screens/login/login.spec.tsx
@@ -1,33 +1,41 @@
 // disable eslint for `any` type assertion of ReactTestInstance
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from 'react';
-import {
-  cleanup,
-  fireEvent,
-  render,
-  waitFor,
-} from '@testing-library/react-native';
+import { cleanup, fireEvent, render } from '@testing-library/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ThemeProvider } from 'styled-components/native';
 import { ActivityIndicator, PressableProps, Text } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { faker } from '@faker-js/faker';
 import { AuthenticationSpy, ValidationSpy } from '../../mocks';
-import { HttpPostClientSpy } from '~/data/mocks';
 import { UnauthorizedError } from '~/domain/errors';
 import theme from '~/presentation/theme';
 import Login from './login';
+import * as hooks from '../../hooks';
+
+jest.mock('../../hooks', () => ({
+  useAuthentication: jest.fn(() => [false, jest.fn()]),
+}));
 
 const withTheme = (component: React.ReactNode) => {
   return <ThemeProvider theme={theme}>{component}</ThemeProvider>;
 };
 
-const makeLoginComponent = () => {
+type Options = {
+  isLoading?: boolean;
+};
+
+const makeLoginComponent = (options: Options = {}) => {
+  jest
+    .spyOn(hooks, 'useAuthentication')
+    .mockImplementationOnce(() => [options.isLoading || false, jest.fn()]);
+
   const authenticationSpy = new AuthenticationSpy();
   const validationSpy = new ValidationSpy();
-  const httpPostClient = new HttpPostClientSpy();
+
   const result = render(
     withTheme(
-      <Login validation={validationSpy} httpPostClient={httpPostClient} />,
+      <Login validation={validationSpy} authentication={authenticationSpy} />,
     ),
   );
 
@@ -35,12 +43,15 @@ const makeLoginComponent = () => {
     result,
     validationSpy,
     authenticationSpy,
-    httpPostClient,
   };
 };
 
 describe('Login Screen', () => {
   afterEach(cleanup);
+  beforeEach(async () => {
+    await AsyncStorage.clear();
+  });
+
   test('Should start with initial state', () => {
     const { result } = makeLoginComponent();
 
@@ -76,82 +87,88 @@ describe('Login Screen', () => {
     expect(passwordInput.props.defaultValue).toBe(password);
   });
 
-  test('Should call validation with correct values', async () => {
+  test('Should call validation with correct values', () => {
     const { result, validationSpy } = makeLoginComponent();
     const primaryButton = result.getByTestId('primary-button');
     const emailInput = result.getByTestId('email-input').findByType(TextInput);
     const email = faker.internet.email();
 
     fireEvent.changeText(emailInput, email);
-    void waitFor(async () => {
-      await fireEvent.press(primaryButton);
-    });
+    fireEvent.press(primaryButton);
 
-    await expect(validationSpy.errorMessage).toBeFalsy(); // eslint-disable-line
+    expect(validationSpy.errorMessage).toBeFalsy();
   });
 
-  test('Should not be able to press the button when disabled', async () => {
-    const { result } = makeLoginComponent();
+  test('Should not be able to press the button when disabled', () => {
+    const { result } = makeLoginComponent({ isLoading: true });
     const primaryButton = result.getByTestId('primary-button');
 
-    void waitFor(async () => {
-      await fireEvent.press(primaryButton);
-    });
+    fireEvent.press(primaryButton);
 
-    /* 
-    This needs to be awaited due to a problem with how fireEvent works.
-    It assumes that everything is done after the event is fired
-    and the component is unmounted before some states updates are done
-    so it'll console.error about memory leak issue with React 
-    */
-    await expect(primaryButton.props.accessibilityState.disabled).toBe(true); // eslint-disable-line
+    expect(primaryButton.props.accessibilityState.disabled).toBe(true);
   });
 
-  test('Should show ActivityIndicator when isLoading is true', async () => {
-    const { result } = makeLoginComponent();
-    const primaryButton = result.getByTestId('primary-button');
+  test('Should show ActivityIndicator when isLoading is true', () => {
+    const { result } = makeLoginComponent({ isLoading: true });
     const activityIndicator = result.getByTestId('activity-indicator');
-
-    void waitFor(async () => {
-      await fireEvent.press(primaryButton);
-    });
     const spinner = activityIndicator.findByType(ActivityIndicator);
 
-    await expect(activityIndicator.children.length).toBe(1); // eslint-disable-line
-    await expect(spinner).toBeTruthy(); // eslint-disable-line
+    expect(activityIndicator.children.length).toBe(1);
+    expect(spinner).toBeTruthy();
   });
 
-  test('Should call Authentication with correct values', async () => {
+  test('Should call Authentication with correct values', () => {
     const email = faker.internet.email();
     const password = faker.internet.password();
-    const { result, validationSpy, authenticationSpy } = makeLoginComponent();
+    const { result, authenticationSpy } = makeLoginComponent();
     const primaryButton = result.getByTestId('primary-button');
 
-    void waitFor(async () => {
-      await fireEvent.press(primaryButton, [
-        validationSpy.validate('email', email),
-        validationSpy.validate('password', password),
-        authenticationSpy.auth({ email, password }),
-      ]);
-    });
+    const emailInput = result.getByTestId('email-input').findByType(TextInput);
+    const passwordInput = result
+      .getByTestId('password-input')
+      .findByType(TextInput);
 
-    await expect(authenticationSpy.params).toEqual({ email, password }); // eslint-disable-line
+    fireEvent.changeText(emailInput, email);
+    fireEvent.changeText(passwordInput, password);
+    fireEvent.press(primaryButton, [
+      authenticationSpy.auth({ email, password }),
+    ]);
+
+    expect(authenticationSpy.params).toEqual({ email, password });
   });
 
-  test('Should throw an error if Authentication fails', async () => {
+  test('Should throw an error if Authentication fails', () => {
     const error = new UnauthorizedError();
     const { result, validationSpy } = makeLoginComponent();
     const primaryButton = result.getByTestId('primary-button');
 
     validationSpy.errorMessage = error.message;
 
-    void waitFor(async () => {
-      await fireEvent.press(primaryButton);
-    });
+    fireEvent.press(primaryButton);
 
     const activityIndicator = result.getByTestId('activity-indicator');
     const errorMessage = activityIndicator.findByType(Text);
 
-    await expect(errorMessage.props.children).toEqual(error.message); // eslint-disable-line
+    expect(errorMessage.props.children).toEqual(error.message);
+  });
+
+  test('Should save accessToken to Async Storage', () => {
+    const accessToken = faker.datatype.uuid();
+    jest.spyOn(hooks, 'useAuthentication').mockImplementationOnce(() => [
+      false,
+      jest.fn(() => {
+        void AsyncStorage.setItem('accessToken', accessToken);
+        return Promise.resolve();
+      }),
+    ]);
+    const { result } = makeLoginComponent();
+    const primaryButton = result.getByTestId('primary-button');
+
+    fireEvent.press(primaryButton);
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      'accessToken',
+      accessToken,
+    );
   });
 });
