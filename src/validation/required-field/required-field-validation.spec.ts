67de0fc6ca950b600bf08beed00bc9c0e895094f
Raul Max <raulmax319@gmail.com>
2022-03-16 16:11:49 -0300

    refactor: makeSut factory method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,17 +1,20 @@
+import { faker } from '@faker-js/faker';
 import { RequiredFieldError } from '../errors';
 import { RequiredFieldValidation } from './required-field-validation';
 
+const makeSut = () => new RequiredFieldValidation(faker.database.column());
+
 describe('RequiredFieldValidation', () => {
   test('Should return an error if the field is empty', () => {
-    const sut = new RequiredFieldValidation('email');
+    const sut = makeSut();
     const error = sut.validate('');
 
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test('Should not return if the field has a value', () => {
-    const sut = new RequiredFieldValidation('email');
-    const error = sut.validate('any_value');
+    const sut = makeSut();
+    const error = sut.validate(faker.random.word());
 
     expect(error).toBeFalsy();
   });
