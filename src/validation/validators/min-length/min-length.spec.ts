8f275d36fb4fb5b5709a052b1ad24b3f09235345
Raul Max <raulmax319@gmail.com>
2022-05-17 13:37:49 -0300

    test: min-length validation

---
+++ src/validation/validators/min-length/min-length.spec.ts
@@ -0,0 +1,16 @@
+import faker from '@faker-js/faker';
+import { InvalidFieldError } from '~/validation/errors';
+import { MinLengthValidation } from './min-length';
+
+const makeSut = (field: string, length: number) =>
+  new MinLengthValidation(field, length);
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is too short', () => {
+    const field = faker.database.column();
+    const sut = makeSut(field, 5);
+    const result = sut.validate('123');
+
+    expect(result).toEqual(new InvalidFieldError(field));
+  });
+});
