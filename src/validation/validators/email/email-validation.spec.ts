a56f53137957331eebdc11bb75443f49466dfcd0
Raul Max <raulmax319@gmail.com>
2022-05-17 11:08:13 -0300

    test: ensure email is validating

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,12 +1,23 @@
+import faker from '@faker-js/faker';
 import { InvalidFieldError } from '~/validation/errors';
 import { EmailValidation } from './email-validation';
 
+const makeSut = () => new EmailValidation('email');
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const email = 'test';
-    const sut = new EmailValidation('email');
+    const email = faker.random.word();
+    const sut = makeSut();
     const result = sut.validate(email);
 
     expect(result).toEqual(new InvalidFieldError('email'));
+  });
+
+  test('Should return not return anything if email is valid', () => {
+    const email = faker.internet.email();
+    const sut = makeSut();
+    const result = sut.validate(email);
+
+    expect(result).toBeFalsy();
   });
 });
