c76b8b15f061a7675c36e477be5161e4a673fc72
Raul Max <raulmax319@gmail.com>
2022-05-24 08:30:16 -0300

    refactor: move sut to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,17 +1,23 @@
 import { FieldValidationSpy } from '../mocks/field-validation.mock';
 import { ValidationComposite } from './validation-composite';
 
+const makeSut = () => {
+  const validators = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field'),
+  ];
+  const sut = new ValidationComposite(validators);
+
+  return { sut, validators };
+};
+
 describe('ValidationComposite', () => {
   test('should return an error if any validation fails', () => {
-    const validationSpy = new FieldValidationSpy('any_field');
-    validationSpy.error = new Error('first_error');
-    const validationSpy2 = new FieldValidationSpy('any_field');
-    validationSpy2.error = new Error('second_error');
-
-    const validators = [validationSpy, validationSpy2];
-    const sut = new ValidationComposite(validators);
+    const { sut, validators } = makeSut();
+    validators[0].error = new Error('first_error');
+    validators[1].error = new Error('second_error');
     const error = sut.validate('any_field', 'any_value');
 
-    expect(error).toBe(validationSpy.error.message);
+    expect(error).toBe(validators[0].error.message);
   });
 });
