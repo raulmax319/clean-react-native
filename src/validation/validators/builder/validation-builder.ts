0614974d4308da10266fd40a7b1eced5b12f6364
Raul Max <raulmax319@gmail.com>
2022-05-24 09:21:01 -0300

    feat: validation builder email helper method

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,5 +1,8 @@
 import { FieldValidation } from '~/validation/protocols/field-validation';
-import { RequiredFieldValidation } from '../required-field/required-field-validation';
+import {
+  RequiredFieldValidation,
+  EmailValidation,
+} from '~/validation/validators';
 
 export class ValidationBuilder {
   private constructor(
@@ -13,6 +16,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validators.push(new RequiredFieldValidation(this.fieldName));
+    return this;
+  }
+
+  email(): ValidationBuilder {
+    this.validators.push(new EmailValidation(this.fieldName));
     return this;
   }
 
