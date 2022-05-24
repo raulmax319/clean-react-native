0750bf4bf3e737fef89e41ea3a0fc983c5f8748d
Raul Max <raulmax319@gmail.com>
2022-05-24 09:38:33 -0300

    refactor: random length size for min length validation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -23,22 +23,24 @@ describe('ValidationBuilder', () => {
 
   test('should return MinLengthValidation', () => {
     const field = faker.database.column();
-    const validators = ValidationBuilder.field(field).min(5).build();
+    const length = faker.datatype.number({ max: 50 });
+    const validators = ValidationBuilder.field(field).min(length).build();
 
-    expect(validators).toEqual([new MinLengthValidation(field, 5)]);
+    expect(validators).toEqual([new MinLengthValidation(field, length)]);
   });
 
   test('should return a list of validators', () => {
     const field = faker.database.column();
+    const length = faker.datatype.number({ max: 50 });
     const validators = ValidationBuilder.field(field)
       .required()
-      .min(5)
+      .min(length)
       .email()
       .build();
 
     expect(validators).toEqual([
       new RequiredFieldValidation(field),
-      new MinLengthValidation(field, 5),
+      new MinLengthValidation(field, length),
       new EmailValidation(field),
     ]);
   });
