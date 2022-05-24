468d64e9eac944909c159f5686cf1a47fb87bc3f
Raul Max <raulmax319@gmail.com>
2022-05-24 09:35:26 -0300

    refactor: random field name with faker

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import faker from '@faker-js/faker';
 import {
   RequiredFieldValidation,
   EmailValidation,
@@ -7,20 +8,38 @@ import { ValidationBuilder } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
-    const validators = ValidationBuilder.field('field').required().build();
+    const field = faker.database.column();
+    const validators = ValidationBuilder.field(field).required().build();
 
-    expect(validators).toEqual([new RequiredFieldValidation('field')]);
+    expect(validators).toEqual([new RequiredFieldValidation(field)]);
   });
 
   test('should return EmailValidation', () => {
-    const validators = ValidationBuilder.field('field').email().build();
+    const field = faker.database.column();
+    const validators = ValidationBuilder.field(field).email().build();
 
-    expect(validators).toEqual([new EmailValidation('field')]);
+    expect(validators).toEqual([new EmailValidation(field)]);
   });
 
   test('should return MinLengthValidation', () => {
-    const validators = ValidationBuilder.field('field').min(5).build();
+    const field = faker.database.column();
+    const validators = ValidationBuilder.field(field).min(5).build();
 
-    expect(validators).toEqual([new MinLengthValidation('field', 5)]);
+    expect(validators).toEqual([new MinLengthValidation(field, 5)]);
+  });
+
+  test('should return a list of validators', () => {
+    const field = faker.database.column();
+    const validators = ValidationBuilder.field(field)
+      .required()
+      .min(5)
+      .email()
+      .build();
+
+    expect(validators).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, 5),
+      new EmailValidation(field),
+    ]);
   });
 });
