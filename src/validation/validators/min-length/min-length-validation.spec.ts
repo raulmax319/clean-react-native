607537ab00037dcb80ac89d45b9bcc4160c7736d
Raul Max <raulmax319@gmail.com>
2022-05-24 07:53:17 -0300

    refactor: return field name on sut

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,21 +2,26 @@ import faker from '@faker-js/faker';
 import { InvalidFieldError } from '~/validation/errors';
 import { MinLengthValidation } from './min-length-validation';
 
-const makeSut = (field: string, length: number) =>
-  new MinLengthValidation(field, length);
+const makeSut = (length: number) => {
+  const field = faker.database.column();
+  const sut = new MinLengthValidation(field, length);
+
+  return {
+    sut,
+    field,
+  };
+};
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is too short', () => {
-    const field = faker.database.column();
-    const sut = makeSut(field, 5);
+    const { sut, field } = makeSut(5);
     const result = sut.validate('123');
 
     expect(result).toEqual(new InvalidFieldError(field));
   });
 
   test('Should not return error if value length is valid', () => {
-    const field = faker.database.column();
-    const sut = makeSut(field, 5);
+    const { sut } = makeSut(5);
     const result = sut.validate('12345');
 
     expect(result).toBeFalsy();
