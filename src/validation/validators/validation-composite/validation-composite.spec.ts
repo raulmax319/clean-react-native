1213c1a8e64d8287840861260ea7e1f10ef00081
Raul Max <raulmax319@gmail.com>
2022-05-24 08:24:46 -0300

    refactor: ensure validation returns the first error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { ValidationComposite } from './validation-composite';
 describe('ValidationComposite', () => {
   test('should return an error if any validation fails', () => {
     const validationSpy = new FieldValidationSpy('any_field');
+    validationSpy.error = new Error('first_error');
     const validationSpy2 = new FieldValidationSpy('any_field');
-    validationSpy2.error = new Error('any_error');
+    validationSpy2.error = new Error('second_error');
 
     const validators = [validationSpy, validationSpy2];
     const sut = new ValidationComposite(validators);
     const error = sut.validate('any_field', 'any_value');
 
-    expect(error).toBe(validationSpy2.error.message);
+    expect(error).toBe(validationSpy.error.message);
   });
 });
