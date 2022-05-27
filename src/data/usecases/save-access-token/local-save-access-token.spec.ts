7745229e452de3083bd9c6660448ecad4069079a
Raul Max <raulmax319@gmail.com>
2022-05-27 17:29:37 -0300

    refactor: move local save access token instance to a factory method

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -3,10 +3,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AsyncStorageClient } from '~/infra/storage/async-storage-client';
 import { LocalSaveAccessToken } from './local-save-access-token';
 
+const makeSut = () => {
+  const storageClient = AsyncStorageClient.shared;
+  return new LocalSaveAccessToken(storageClient);
+};
+
 describe('LocalSaveAccessToken', () => {
   test('should call save method from storage with correct values', async () => {
-    const storageClient = AsyncStorageClient.shared;
-    const sut = new LocalSaveAccessToken(storageClient);
+    const sut = makeSut();
 
     const accessToken = faker.datatype.uuid();
     await sut.save(accessToken);
