4741b589fd88ac85fce2e158f2674935b6cb5be3
Raul Max <raulmax319@gmail.com>
2022-05-26 21:13:19 -0300

    fix: styled components module theme declaration

--- src/main/config/styled-components-module.d.ts
+++ src/main/config/styled-components-module.d.ts
@@ -1,7 +1,7 @@
 import 'styled-components';
 import { colors } from '~/presentation/theme';
 
-declare module 'styled-components' {
+declare module 'styled-components/native' {
   export interface DefaultTheme {
     colors: typeof colors;
   }
