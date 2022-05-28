974dcab707d036f546abc0f6506cae13aa776261
Raul Max <raulmax319@gmail.com>
2022-05-28 14:07:27 -0300

    fix: login validatidation coverage

--- src/presentation/mocks/validation.mock.ts
+++ src/presentation/mocks/validation.mock.ts
@@ -4,10 +4,15 @@ export class ValidationSpy implements Validation {
   field: string;
   value: string;
   errorMessage: string;
+  shouldPass = false;
 
   validate(field: string, value: string): string {
     this.field = field;
     this.value = value;
+    if (this.shouldPass) {
+      if (field === 'email') return null;
+    }
+
     return this.errorMessage;
   }
 }
